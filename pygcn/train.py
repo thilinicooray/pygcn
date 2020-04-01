b64f7914dab79d07f07f7de72a21f58b3c763f89
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-01 17:44:27 +0800

    res con

--- pygcn/train.py
+++ pygcn/train.py
@@ -33,7 +33,7 @@ parser.add_argument('--dropout', type=float, default=0.5,
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
-np.random.seed(args.seed)
+#np.random.seed(args.seed)
 torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
