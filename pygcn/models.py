cc116adc08824d66880b7a72efe02407620ee721
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-03 11:03:27 +0800

    res gcn

--- pygcn/models.py
+++ pygcn/models.py
@@ -18,12 +18,12 @@ class GCN(nn.Module):
     def forward(self, x_org, adj):
         x_org = self.encoder(x_org)
         x = F.relu(self.gc1(x_org, adj))
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = x + x_org
+        x1 = F.dropout(x, self.dropout, training=self.training)
+        x = x1 + x_org
         #x = self.combiner(torch.cat([x, x_org], -1))
         x = self.gc2(x, adj)
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = x + x_org
+        x2 = F.dropout(x, self.dropout, training=self.training)
+        x = x1 + x2
         #x = self.combiner(torch.cat([x, x_org], -1))
         x = self.gc3(x, adj)
 
