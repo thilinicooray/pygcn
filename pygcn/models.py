880101e3e925c6211fa6e8ebaf11fababd9cf17d
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-03 09:45:13 +0800

    check ad

--- pygcn/models.py
+++ pygcn/models.py
@@ -11,21 +11,17 @@ class GCN(nn.Module):
         self.gc1 = GraphConvolution(nfeat, nhid)
         self.gc2 = GraphConvolution(nhid, nhid)
         self.gc3 = GraphConvolution(nhid, nclass)
-        self.encoder1 = nn.Parameter(torch.zeros(size=(nfeat, nhid)))
-        self.encoder = GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=0.2, concat=False)
         self.combiner = nn.Parameter(torch.zeros(size=(nhid + nhid, nhid)))
         self.dropout = dropout
 
     def forward(self, x_org, adj):
-        x_proj = self.encoder1(x_org)
-        x_entire = self.encoder(x_proj)
-        x_entire = x_proj * x_entire
-        x = F.relu(self.gc1(x_entire, adj))
+        print('came here')
+        x = F.relu(self.gc1(x_org, adj))
         x = F.dropout(x, self.dropout, training=self.training)
-        x = self.combiner(torch.cat([x, x_entire], -1))
+        x = self.combiner(torch.cat([x, x_org], -1))
         x = self.gc2(x, adj)
         x = F.dropout(x, self.dropout, training=self.training)
-        x = self.combiner(torch.cat([x, x_entire], -1))
+        x = self.combiner(torch.cat([x, x_org], -1))
         x = self.gc3(x, adj)
 
         return F.log_softmax(x, dim=1)
