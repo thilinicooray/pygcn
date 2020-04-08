c28a244ae791997f78ce81705157953c5a000570
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-08 10:53:53 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -19,6 +19,6 @@ class GCN(nn.Module):
         x_init = self.embedding_h(x)
         x = F.relu(self.gc1(x_init, adj))
         x = F.dropout(x, self.dropout, training=self.training)
-        x = torch.tanh(F.tanh(self.joint(x + x_init))
+        x = torch.tanh(F.tanh(self.joint(x + x_init)))
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
