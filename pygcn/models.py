cb50d16141fce60f8403aa54132ceb8f2fe6ef2e
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-08 11:25:12 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -7,12 +7,9 @@ class GCN(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout):
         super(GCN, self).__init__()
 
-        self.gc1 = GraphConvolution(nhid, nhid)
+        self.gc1 = GraphConvolution(nfeat, nhid)
         self.gc2 = GraphConvolution(nhid, nclass)
         self.dropout = dropout
-
-        self.embedding_h = nn.Linear(nfeat, nhid)
-        self.joint = nn.Linear(nhid, nhid)
 
     def forward(self, x, adj):
         x = F.relu(self.gc1(x, adj))
