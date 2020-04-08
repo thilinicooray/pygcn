f9b2f67fa5d263500cbc1896d9687e68051789d2
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-08 11:21:02 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from pygcn.layers import GraphConvolution
@@ -11,15 +10,12 @@ class GCN(nn.Module):
         self.gc1 = GraphConvolution(nhid, nhid)
         self.gc2 = GraphConvolution(nhid, nclass)
         self.dropout = dropout
-        print('dropout ', dropout)
 
         self.embedding_h = nn.Linear(nfeat, nhid)
         self.joint = nn.Linear(nhid, nhid)
 
     def forward(self, x, adj):
-        x_init = self.embedding_h(x)
-        x = F.relu(self.gc1(x_init, adj))
+        x = F.relu(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
-        x = torch.tanh(F.tanh(self.joint(x + x_init)))
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
