dd583820631f595d1436e561479d9f22cb6ac09a
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-08 10:47:27 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -12,11 +12,12 @@ class GCN(nn.Module):
         self.dropout = dropout
 
         self.embedding_h = nn.Linear(nfeat, nhid)
+        self.joint = nn.Linear(nhid, nhid)
 
     def forward(self, x, adj):
         x_init = self.embedding_h(x)
         x = F.relu(self.gc1(x_init, adj))
         x = F.dropout(x, self.dropout, training=self.training)
-        x = x + x_init
+        x = self.joint(x + x_init)
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
