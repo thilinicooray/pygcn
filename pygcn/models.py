b6d3dc1a7cf64ee5bee7a3146c7ecdda3b98bec7
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-11 15:07:49 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -15,10 +15,10 @@ class GCN(nn.Module):
         self.dropout = dropout
 
     def forward(self, x, adj, adj1, fully_connected_graph):
-        x = self.emb(x)
+        x_init = self.emb(x)
         #making edge features
-        conv1 = x.unsqueeze(1).expand(adj.size(0), adj.size(0), x.size(-1))
-        conv2 = x.unsqueeze(0).expand(adj.size(0), adj.size(0), x.size(-1))
+        conv1 = x_init.unsqueeze(1).expand(adj.size(0), adj.size(0), x_init.size(-1))
+        conv2 = x_init.unsqueeze(0).expand(adj.size(0), adj.size(0), x_init.size(-1))
         conv1 = conv1.contiguous().view(-1, x.size(-1))
         conv2 = conv2.contiguous().view(-1, x.size(-1))
 
@@ -26,9 +26,9 @@ class GCN(nn.Module):
 
         x_e = F.relu(self.gc_e(edge_feat, adj1))
         x_e = F.dropout(x_e, self.dropout, training=self.training)
-        x = F.relu(self.gc1(x, adj))
+        x = F.relu(self.gc1(x_init, adj))
         x = F.dropout(x, self.dropout, training=self.training)
         #x = self.joint(x)
-        x = x + x_e
+        x = x + x_e + x_init
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
