ed766d8b5f8e4d5ab91ca4d9d32dbc11c9960f16
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-13 13:10:38 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -11,7 +11,8 @@ class GCN(nn.Module):
         self.gc1 = GraphConvolution(nfeat, nhid)
         self.gc_e = GraphConvolution_edge(nhid*2, nhid)
         self.gc_e2 = GraphConvolution_edge(nhid*2, nhid)
-        self.gc2 = GraphConvolution(nhid*2, nclass)
+        self.gc3 = GraphConvolution(nhid, nhid)
+        self.gc2 = GraphConvolution(nhid, nclass)
         self.emb = nn.Linear(nfeat, nhid)
         self.joint = nn.Linear(nhid, nhid)
         self.dropout = dropout
@@ -69,10 +70,15 @@ class GCN(nn.Module):
 
         x = F.relu(self.gc1(x_init, adj1))
         x = F.dropout(x, self.dropout, training=self.training)
+
+        x = F.relu(self.gc3(x, adj1))
+        x = F.dropout(x, self.dropout, training=self.training)
+
+
         #x = torch.cat([x_init, x], -1)
         #x = x + x_init
 
-        conv1 = x.unsqueeze(1).expand(adj.size(0), adj.size(0), x.size(-1))
+        '''conv1 = x.unsqueeze(1).expand(adj.size(0), adj.size(0), x.size(-1))
         conv2 = x.unsqueeze(0).expand(adj.size(0), adj.size(0), x.size(-1))
         conv1 = conv1.contiguous().view(-1, x.size(-1))
         conv2 = conv2.contiguous().view(-1, x.size(-1))
@@ -84,7 +90,7 @@ class GCN(nn.Module):
 
 
 
-        x = self.gc2(torch.cat([x,  x_e],-1), adj1)
-        #x = self.gc2(x, adj1)
+        x = self.gc2(torch.cat([x,  x_e],-1), adj1)'''
+        x = self.gc2(x, adj1)
         return F.log_softmax(x, dim=1)
 
