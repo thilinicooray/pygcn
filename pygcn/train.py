cb492814ce365a6ce50447355ceda64affb598c2
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-13 12:32:22 +0800

    add input

--- pygcn/train.py
+++ pygcn/train.py
@@ -21,8 +21,6 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
 parser.add_argument('--fastmode', action='store_true', default=False,
                     help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=200,
-                    help='Number of epochs to train.')
 parser.add_argument('--lr', type=float, default=0.01,
                     help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4,
@@ -31,6 +29,8 @@ parser.add_argument('--hidden', type=int, default=16,
                     help='Number of hidden units.')
 parser.add_argument('--dropout', type=float, default=0.5,
                     help='Dropout rate (1 - keep probability).')
+parser.add_argument('--patience', type=int, default=100, help='Patience')
+parser.add_argument('--epochs', type=int, default=10000, help='Number of epochs to train.')
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
