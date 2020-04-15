182d7bccb7903c961ee6f038648d15fa8cbb941d
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-15 14:22:26 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -12,7 +12,7 @@ class GCN(nn.Module):
         self.gc_e = GraphConvolution_edge(nhid*2, nhid)
         self.gc_e2 = GraphConvolution_edge(nhid*2, nhid)
         self.gc_e3 = GraphConvolution_edge(nhid*2, nhid)
-        self.gc3 = GraphConvolution(nhid, nhid)
+        self.gc3 = GraphConvolution(nhid + nfeat, nhid)
         self.gc2 = GraphConvolution(nhid*2 + nfeat, nclass)
         self.emb = nn.Linear(nfeat, nhid)
         self.joint = nn.Linear(nhid + nfeat, nhid)
