ad9dd25453a09e9b1e00ce3eebd1800b795ab580
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-19 20:38:49 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -99,7 +99,7 @@ class GCN(nn.Module):
         scores = edge_feat.masked_fill(edge_feat > 0, 1).squeeze()
         adj1 = adj1_org * scores
 
-        #adj1 = adj1
+        adj1 = adj1 + adj1_org
 
         x = torch.cat([x, x_init], -1)
 
