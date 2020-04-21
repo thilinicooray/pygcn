953253ca33a7f5906930ea1eb4ab831bba8ae086
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-21 15:25:50 +0800

    add input

--- pygcn/train.py
+++ pygcn/train.py
@@ -60,7 +60,7 @@ fully_connected_graph = torch.ones(adj.size(0), adj.size(0))
 for idx1 in range(0,adj.size(0)):
     fully_connected_graph[idx1][idx1] = 0
 
-for k in range(1):
+for k in range(10):
 
     # Model and optimizer
     model = GCNModelVAE(input_feat_dim=features.shape[1],
@@ -140,8 +140,8 @@ for k in range(1):
         node_cls_loss_train = F.nll_loss(output[idx_train], labels[idx_train])
         ae_loss = loss_function(preds=recovered, labels=adj1,
                              mu=mu, logvar=logvar, n_nodes=features.shape[0])
-        print('losses ', node_cls_loss_train, ae_loss)
-        loss_train = node_cls_loss_train + ae_loss
+        #print('losses ', node_cls_loss_train, ae_loss)
+        loss_train = 0.5 * node_cls_loss_train + ae_loss
         acc_train = accuracy(output[idx_train], labels[idx_train])
         loss_train.backward()
         optimizer.step()
@@ -154,12 +154,12 @@ for k in range(1):
 
         loss_val = F.nll_loss(output[idx_val], labels[idx_val])
         acc_val = accuracy(output[idx_val], labels[idx_val])
-        print('Epoch: {:04d}'.format(epoch+1),
+        '''print('Epoch: {:04d}'.format(epoch+1),
               'loss_train: {:.4f}'.format(loss_train.item()),
               'acc_train: {:.4f}'.format(acc_train.item()),
               'loss_val: {:.4f}'.format(loss_val.item()),
               'acc_val: {:.4f}'.format(acc_val.item()),
-              'time: {:.4f}s'.format(time.time() - t))
+              'time: {:.4f}s'.format(time.time() - t))'''
 
         #return loss_val.data.item()
         return acc_val.data.item()
