571b148176a9d10dbee13718ce338aa3668ef09a
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-21 17:39:29 +0800

    add input

--- pygcn/train.py
+++ pygcn/train.py
@@ -6,7 +6,6 @@ import argparse
 import numpy as np
 
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.modules.loss
 import torch.optim as optim
@@ -138,13 +137,11 @@ for k in range(10):
         model.train()
         optimizer.zero_grad()
         recovered, mu, logvar, output = model(features, adj1)
-        #node_cls_loss_train = F.nll_loss(output[idx_train], labels[idx_train])
-        criterion = nn.CrossEntropyLoss()
-        node_cls_loss_train = criterion(output[idx_train], labels[idx_train])
+        node_cls_loss_train = F.nll_loss(output[idx_train], labels[idx_train])
         ae_loss = loss_function(preds=recovered[idx_train], labels=adj1[idx_train],
                              mu=mu, logvar=logvar, n_nodes=features.shape[0])
         #print('losses ', node_cls_loss_train, ae_loss)
-        loss_train = node_cls_loss_train + 0.5*ae_loss
+        loss_train = node_cls_loss_train + 0.25*ae_loss
         acc_train = accuracy(output[idx_train], labels[idx_train])
         loss_train.backward()
         optimizer.step()
