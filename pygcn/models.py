e4d914501e29ac0c150a04a05d284ba207f56d35
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-22 15:10:57 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -11,7 +11,7 @@ class GCN(nn.Module):
         self.gc2 = GraphConvolution1(nhid, nclass)
         self.dropout = dropout
 
-    def forward(self, x, adj, adj1, fc):
+    def forward(self, x, adj):
         x = F.relu(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
