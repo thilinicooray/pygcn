934d7ab3abfb95b49acbb0e3a55f1e9aed6b1976
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-22 10:32:46 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -53,11 +53,11 @@ class GCNModelVAE(nn.Module):
     def forward(self, x, adj):
         mu, logvar, layer1rep = self.encode(x, adj)
         z = self.reparameterize(mu, logvar)
-        pred_a = self.dc(z)
+        pred_a1 = self.dc(z)
 
         #get masked new adj
-        zero_vec = -9e15*torch.ones_like(pred_a)
-        masked_adj = torch.where(adj > 0, pred_a, zero_vec)
+        zero_vec = -9e15*torch.ones_like(pred_a1)
+        masked_adj = torch.where(adj > 0, pred_a1, zero_vec)
         new_adj = F.softmax(masked_adj, dim=1)
 
         hidden2 = self.gc2_1(torch.cat([x, layer1rep], -1), new_adj)
@@ -66,9 +66,9 @@ class GCNModelVAE(nn.Module):
         z = self.reparameterize(mu, logvar)
         pred_a = self.dc1(z)
 
-        classifier = self.gc_class(hidden2, new_adj)
+        classifier = self.gc_class(hidden2 + layer1rep, new_adj)
 
-        return pred_a, mu, logvar, F.log_softmax(classifier, dim=1)
+        return pred_a+pred_a1, mu, logvar, F.log_softmax(classifier, dim=1)
 
 
 class InnerProductDecoder(nn.Module):
