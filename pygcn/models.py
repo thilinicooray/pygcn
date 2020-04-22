1701bbbb5e23b68e150c54b05b9c20b8dcc4faa0
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-22 13:12:34 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -33,16 +33,9 @@ class GCNModelVAE(nn.Module):
         self.dc = InnerProductDecoder(dropout, act=lambda x: x)
         self.dc1 = InnerProductDecoder(dropout, act=lambda x: x)
 
-        self.gc2_1 = GraphConvolution(input_feat_dim, hidden_dim1, dropout, act=F.relu)
+        self.gc2_1 = GraphConvolution(hidden_dim1, hidden_dim1, dropout, act=F.relu)
         self.gc2_2 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
         self.gc2_3 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
-
-        self.node_decoder = nn.Sequential(
-            nn.Linear(2708, hidden_dim2, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(hidden_dim2, input_feat_dim, bias=False),
-            nn.ReLU(inplace=True)
-        )
         self.gc_class = GraphConvolution(hidden_dim1, nclass)
 
     def encode(self, x, adj):
@@ -61,17 +54,24 @@ class GCNModelVAE(nn.Module):
         mu, logvar, layer1rep = self.encode(x, adj)
         z = self.reparameterize(mu, logvar)
         pred_a1 = self.dc(z)
-        decoded_noderep = self.node_decoder(pred_a1)
+
 
         #get masked new adj
-        zero_vec = -9e15*torch.ones_like(pred_a1)
+        '''zero_vec = -9e15*torch.ones_like(pred_a1)
         masked_adj = torch.where(adj > 0, pred_a1, zero_vec)
         new_adj = F.softmax(masked_adj, dim=1)
 
-        hidden2 = self.gc2_1(decoded_noderep, new_adj)
+        hidden2 = self.gc2_1(torch.cat([x, layer1rep], -1), new_adj)
 
 
-        classifier = self.gc_class(hidden2, adj)
+
+
+        mu = self.gc2_2(hidden2, new_adj)
+        logvar = self.gc2_3(hidden2, new_adj)
+        z = self.reparameterize(mu, logvar)
+        pred_a = self.dc1(z)'''
+
+        classifier = self.gc_class(layer1rep, adj)
 
         return pred_a1, mu, logvar, F.log_softmax(classifier, dim=1)
 
