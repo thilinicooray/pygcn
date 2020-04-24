811c6e8ae7e48cdf475d340d02d508ec09ba2d3a
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-24 18:31:11 +0800

    add input

--- pygcn/train.py
+++ pygcn/train.py
@@ -33,7 +33,7 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
 parser.add_argument('--fastmode', action='store_true', default=False,
                     help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-parser.add_argument('--lr', type=float, default=0.01,
+parser.add_argument('--lr', type=float, default=0.001,
                     help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4,
                     help='Weight decay (L2 loss on parameters).')
