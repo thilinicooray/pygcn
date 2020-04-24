ee9b400302033921509cf65af765824908edbcad
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-04-24 19:32:48 +0800

    add input

--- pygcn/models.py
+++ pygcn/models.py
@@ -16,6 +16,10 @@ class GCN(nn.Module):
     def forward(self, x, adj):
         x = F.relu(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc3(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc4(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
 
